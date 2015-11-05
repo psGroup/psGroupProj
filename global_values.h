2e6dd5b81569357ae90c05ca3728ce5a5104bbd2
psGroupProject <s1538435@sms.ed.ac.uk>
2015-11-05 15:26:27 +0000

    Create global_values.h
    all our parameters and constants

---
+++ global_values.h
@@ -0,0 +1,28 @@
+#ifndef GLOBAL_VALUES_H
+#define GLOBAL_VALUES_H
+
+
+extern double dt;
+extern double r;
+extern double a;
+extern double b;
+extern double m;
+extern double k;
+extern double l;
+extern double T;
+extern double crit_hares_lower;
+extern double crit_hares_upper;
+extern double crit_pumas_lower;
+extern double crit_pumas_upper;
+
+
+/**
+*setting the colour scales for the output ppm files
+*/
+extern double min_colour;
+extern double scale_pumas;
+extern double scale_hares;
+
+
+#endif /* GLOBAL_VALUES */
+
